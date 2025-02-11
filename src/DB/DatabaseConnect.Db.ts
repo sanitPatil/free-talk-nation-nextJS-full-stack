7f461cb7b68fcc01e4f7c7568db334d5ccdcfb89
sanitPatil <raghavpatilrp5@gmail.com>
2025-02-11 18:07:51 +0530

    database connection configure

---
+++ src/DB/DatabaseConnect.Db.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+type connection = {
+  isConnected?: boolean;
+};
+
+const connObject: connection = {};
+const DbConnect = async () => {
+  if (connObject.isConnected) {
+    console.log("database already connected!!!");
+    return;
+  }
+
+  try {
+    const dbConnectionResponse = await mongoose.connect(
+      `${process.env.DB_CONNECT_URL}/${process.env.DATABASE_NAME}`
+    );
+
+    console.log(dbConnectionResponse);
+    if (dbConnectionResponse.connection.readyState === 1) {
+      connObject.isConnected = true;
+    }
+
+    console.log("DB Connected Successfully");
+    return;
+  } catch (error) {
+    console.error(`failed to connect db ${error}`);
+    process.exit(1);
+  }
+};
+
+export default DbConnect;
