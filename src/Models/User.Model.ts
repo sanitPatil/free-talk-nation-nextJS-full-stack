75aa8a801f55274d3e7b23c7107929226d43fb0a
sanitPatil <raghavpatilrp5@gmail.com>
2025-02-11 13:03:49 +0530

    user model done

---
+++ src/Models/User.Model.ts
@@ -0,0 +1,66 @@
+import mongoose, { Document, Schema, CallbackError } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  bio: string;
+  profileImage: string;
+  coverImage: string;
+}
+
+const UserSchema: Schema<User> = new Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "username must be unique!"],
+      index: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "username must be unique!"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre<User>(
+  "save",
+  async function (next: (error?: CallbackError) => void) {
+    if (!this.isModified("password")) return next();
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+      next();
+    } catch (error) {
+      console.log("error 56: ", error);
+      next(error as CallbackError);
+    }
+  }
+);
+const userModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("userModel", UserSchema);
+
+export default userModel;
